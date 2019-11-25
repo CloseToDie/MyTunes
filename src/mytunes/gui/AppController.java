4bc24c6abc2c1b75feebc3e0676f0a25005ccc7c
Lumby98 <lumby89@gmail.com>
2019-11-25 11:49:26 +0100

    gui push

--- src/mytunes/gui/AppController.java
+++ src/mytunes/gui/AppController.java
@@ -10,7 +10,13 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.Slider;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
 
 /**
  *
@@ -18,10 +24,46 @@ import javafx.scene.control.Label;
  */
 public class AppController implements Initializable {
     
-    @FXML
     private Label label;
-    
     @FXML
+    private ListView<?> playlist;
+    @FXML
+    private ListView<?> Songs;
+    @FXML
+    private ListView<?> SongsInPlaylist;
+    @FXML
+    private TextField Search;
+    @FXML
+    private Button ToPlaylist;
+    @FXML
+    private Button NewPlaylist;
+    @FXML
+    private Button EditPlaylist;
+    @FXML
+    private Button deletePlaylist;
+    @FXML
+    private Button Up;
+    @FXML
+    private Button Down;
+    @FXML
+    private Button RemoveSongFromPlaylist;
+    @FXML
+    private Button newSong;
+    @FXML
+    private Button EditSong;
+    @FXML
+    private Button DeleteSong;
+    @FXML
+    private Button Exit;
+    @FXML
+    private Button previous;
+    @FXML
+    private Button Play;
+    @FXML
+    private Button Skip;
+    @FXML
+    private Slider volume;
+    
     private void handleButtonAction(ActionEvent event) {
         System.out.println("You clicked me!");
         label.setText("Hello World!");
@@ -31,5 +73,85 @@ public class AppController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void Search(KeyEvent event)
+    {
+    }
+
+    @FXML
+    private void ToPlaylist(ActionEvent event)
+    {
+    }
+
+    @FXML
+    private void NewPlaylist(ActionEvent event)
+    {
+    }
+
+    @FXML
+    private void EditPlaylist(ActionEvent event)
+    {
+    }
+
+    @FXML
+    private void deletePlaylist(ActionEvent event)
+    {
+    }
+
+    @FXML
+    private void goUp(ActionEvent event)
+    {
+    }
+
+    @FXML
+    private void GoDown(ActionEvent event)
+    {
+    }
+
+    @FXML
+    private void RemoveSongFromPlaylist(ActionEvent event)
+    {
+    }
+
+    @FXML
+    private void newSon(ActionEvent event)
+    {
+    }
+
+    @FXML
+    private void EditSong(ActionEvent event)
+    {
+    }
+
+    @FXML
+    private void DeleteSong(ActionEvent event)
+    {
+    }
+
+    @FXML
+    private void Exit(ActionEvent event)
+    {
+    }
+
+    @FXML
+    private void previous(ActionEvent event)
+    {
+    }
+
+    @FXML
+    private void Play(ActionEvent event)
+    {
+    }
+
+    @FXML
+    private void Skip(ActionEvent event)
+    {
+    }
+
+    @FXML
+    private void changeVolume(MouseEvent event)
+    {
+    }
     
 }
